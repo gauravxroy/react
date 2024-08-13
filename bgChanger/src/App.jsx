5ef31c3fa23857fadeb94b8c1a7c0f39f3e2efbc
Gaurav Roy <96660449+gauravxroy@users.noreply.github.com>
2024-08-13 21:17:12 +0530

    Color Changing buttons added

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("#334155");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" w-fll h-screen " style={{ backgroundColor: color }}>
+      <div className="  text-red-400 flex fixed bottom-14 inset-x-0 flex-wrap px-2 justify-center">
+        <div className="flex flex-wrap justify-center text-white bg-white gap-3 shadow-lg px-4 py-3 rounded-2xl">
+          <button
+            onClick={() => setColor("#ef4444")} //we can also use arrow function and pass change value direct aur create a new function and simply pass in onClick={}
+            className="bg-red-500 outline-none rounded-lg px-4 py-2"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("#22c55e")}
+            className="bg-green-500 outline-none rounded-lg px-4 py-2"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("#3b82f6")}
+            className="bg-blue-500 outline-none rounded-lg px-4 py-2"
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("#111827")}
+            className="bg-gray-900 outline-none rounded-lg px-4 py-2"
+          >
+            Gray
+          </button>
+          <button
+            onClick={() => setColor("#eab308")}
+            className="bg-yellow-500 outline-none rounded-lg px-4 py-2"
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("#7dd3fc")}
+            className="bg-blue-300 outline-none rounded-lg px-4 py-2"
+          >
+            Sky Blue
+          </button>
+          <button
+            onClick={() => setColor("#fb923c")}
+            className="bg-orange-400 outline-none rounded-lg px-4 py-2"
+          >
+            Orange
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
